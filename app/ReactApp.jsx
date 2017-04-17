760adb4602ddb3768facef8b5b67ded25918d020
Scoopadoop <giesfeldte0427@uwstout.edu>
2017-04-17 14:59:38 -0500

    other changes wtha werent immediately saved

--- app/ReactApp.jsx
+++ app/ReactApp.jsx
@@ -2,34 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import MyComponent from './components/HelloComponent.jsx';
-
-const navBarStyle = {
-  position: 'fixed',
-  left: 0,
-  top: 0,
-  width: '100%'
-};
-
+import NavBar from './components/NavBar.jsx';
 
 const element = (
   <div>
-    <div style={navBarStyle} className="w3-bar w3-light-grey">
-      <a href="#" className="w3-bar-item w3-button">Home</a>
-      <a href="#" className="w3-bar-item w3-button">Link 1</a>
-      <div className="w3-dropdown-hover">
-        <button className="w3-button">Sort By</button>
-        <div className="w3-dropdown-content w3-bar-block w3-card-4">
-          <a href="#" className="w3-bar-item w3-button">Name</a>
-          <a href="#" className="w3-bar-item w3-button">Date</a>
-          <a href="#" className="w3-bar-item w3-button">Professor</a>
-          <a href="#" className="w3-bar-item w3-button">Genre</a>
-          <a href="#" className="w3-bar-item w3-button">Class No.</a>
-          <a href="#" className="w3-bar-item w3-button">Id</a>
-          <a href="#" className="w3-bar-item w3-button">Platform</a>
-        </div>
-      </div>
-      <a href="#" className="w3-bar-item w3-button">Filter By</a>
-    </div>
+    <NavBar />
     <MyComponent asPageHeader message="Hello from ReactApp.jsx" />
     <MyComponent message="This is the second header" />
   </div>
