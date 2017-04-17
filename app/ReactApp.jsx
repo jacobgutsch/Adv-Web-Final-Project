6e8c943046a67661f8898e268cf0134a2e271596
Scoopadoop <giesfeldte0427@uwstout.edu>
2017-04-17 14:42:01 -0500

    changes again

--- app/ReactApp.jsx
+++ app/ReactApp.jsx
@@ -10,26 +10,20 @@ const navBarStyle = {
   width: '100%'
 };
 
-const dropbtnStyle = {
-  display: 'inline-block',
-  color: 'white',
-  padding: '14px 16px',
-};
 
 const element = (
   <div>
-    <div style={navBarStyle} className="w3-bar w3-black">
+    <div style={navBarStyle} className="w3-bar w3-light-grey">
       <a href="#" className="w3-bar-item w3-button">Home</a>
-      <a href="#" className="w3-bar-item w3-dropdown-hover">
-        <button className="w3-button">Hover Over Me!</button>
-        <div style={dropbtnStyle} className="w3-dropdown-content w3-bar-block w3-border">
+      <a href="#" className="w3-bar-item w3-button">Link 1</a>
+      <div className="w3-dropdown-hover">
+        <button className="w3-button">Dropdown</button>
+        <div className="w3-dropdown-content w3-bar-block w3-card-4">
           <a href="#" className="w3-bar-item w3-button">Link 1</a>
           <a href="#" className="w3-bar-item w3-button">Link 2</a>
           <a href="#" className="w3-bar-item w3-button">Link 3</a>
         </div>
-      </a>
-      <a href="#" className="w3-bar-item w3-button">Link 2</a>
-      <a href="#" className="w3-bar-item w3-button">Link 3</a>
+      </div>
     </div>
     <MyComponent asPageHeader message="Hello from ReactApp.jsx" />
     <MyComponent message="This is the second header" />
