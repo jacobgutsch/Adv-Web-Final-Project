e6a779b5e12238d78798fdcf7977db37e9cde9ba
Jacob Gutsch <jgutsch95@gmail.com>
2017-04-19 15:26:35 -0500

    Added couchdb connections

--- config.js
+++ config.js
@@ -7,8 +7,15 @@ module.exports = {
   host: env.HOST || '0.0.0.0',
   browserSyncPort: 9000,
 
+  couchDBPort: 5984,
+  couchDBHost: '127.0.0.1',
+
   // Return a fully qualified URL with host and port
   get serverUrl() {
     return `http://${this.host}:${this.port}`;
+  },
+  // Return CouchDB URL with Couch host and port
+  get couchDBUrl() {
+    return `${this.couchDBHost}:${this.couchDBPort}`;
   }
 };
