c156599974c4af51259151371a6936541b4dc03d
Scoopadoop <giesfeldte0427@uwstout.edu>
2017-04-19 13:31:39 -0500

    I got the fixed nav bar working with drobdown

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -12,22 +12,24 @@ class NavBar extends React.Component {
     };
     return (
       <div  className='w3-container'>
-        <div style={navBarStyle} className="w3-bar w3-light-grey">
-          <a href="#" className="w3-bar-item w3-button">Home</a>
-          <a href="#" className="w3-bar-item w3-button">Link 1</a>
-          <div className="w3-dropdown-hover">
-            <button className="w3-button">Sort By</button>
-            <div className="w3-dropdown-content w3-bar-block w3-card-4">
-              <a href="#" className="w3-bar-item w3-button">Name</a>
-              <a href="#" className="w3-bar-item w3-button">Date</a>
-              <a href="#" className="w3-bar-item w3-button">Professor</a>
-              <a href="#" className="w3-bar-item w3-button">Genre</a>
-              <a href="#" className="w3-bar-item w3-button">Class No.</a>
-              <a href="#" className="w3-bar-item w3-button">Id</a>
-              <a href="#" className="w3-bar-item w3-button">Platform</a>
+        <div className="w3-top">
+          <div className="w3-bar w3-light-grey">
+            <a href="#" className="w3-bar-item w3-button">Home</a>
+            <a href="#" className="w3-bar-item w3-button">Link 1</a>
+            <div className="w3-dropdown-hover">
+              <button className="w3-button">Sort By</button>
+              <div className="w3-dropdown-content w3-bar-block w3-card-4">
+                <a href="#" className="w3-bar-item w3-button">Name</a>
+                <a href="#" className="w3-bar-item w3-button">Date</a>
+                <a href="#" className="w3-bar-item w3-button">Professor</a>
+                <a href="#" className="w3-bar-item w3-button">Genre</a>
+                <a href="#" className="w3-bar-item w3-button">Class No.</a>
+                <a href="#" className="w3-bar-item w3-button">Id</a>
+                <a href="#" className="w3-bar-item w3-button">Platform</a>
+              </div>
             </div>
+            <a href="#" className="w3-bar-item w3-button">Filter By</a>
           </div>
-          <a href="#" className="w3-bar-item w3-button">Filter By</a>
         </div>
       </div>
     );
